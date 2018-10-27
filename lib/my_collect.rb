fc26db5dda11b96bc7a8e5d28fce3a87ebee3aaf
Roberto Samia <bojosteph@gmail.com>
2018-10-27 23:51:32 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+def my_collect(array)
+  x = 0
+  new_array = []
+  while x < array.length
+    new_array << yield(array[x])
+    x += 1
+  end
+  new_array
+end
 
+languages = ['ruby', 'javascript', 'python', 'objective-c']
 
+my_collect(languages) do |language|
+      language.upcase
+end
